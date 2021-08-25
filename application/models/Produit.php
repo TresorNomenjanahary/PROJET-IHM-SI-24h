ca026c66ab88229071ddce999f9df9996cef1adb
finalS3p13 <80745814+finalS3p13@users.noreply.github.com>
2021-08-25 12:08:41 +0300

    Add files via upload

--- application/models/Produit.php
+++ application/models/Produit.php
@@ -13,32 +13,28 @@ class Produit extends CI_Model{
 		return $produit;
 	}
 
-	public function getProduitLegume(){
-
-		$sql = 'SELECT * FROM Produit WHERE idCategorie=3';
-		$query = $this->db->query($sql);
-		$produit = array();
-		foreach ($query->result_array() as $key) {
-			$produit[] = $key;
-		}
-		return $produit;
+	public function insert_Produit($idCategorie,$codeProduit$prixUnitaire,$designation){
+		$sql = "INSERT INTO Produit VALUES (null,%s,%s,%s,%s,%s)";
+		$sql = sprintf($sql,$this->db->escape($idCategorie),$this->db->escape($codeProduit),$this->db->escape($prixUnitaire),$this->db->escape($designation));
+		$this->db->query($sql);
 	}
 
-	public function getProduitdivers(){
-
-		$sql = 'SELECT * FROM Produit WHERE idCategorie=1';
-		$query = $this->db->query($sql);
-		$produit = array();
-		foreach ($query->result_array() as $key) {
-			$produit[] = $key;
-		}
-		return $produit;
+	public function delete_Produit($idProduit){
+		$sql = "DELETE * from Produit where idProduit = %s";
+		$sql = sprintf($sql,$this->db->escape($idProduit));
+		$this->db->query($sql);
 	}
 
-	public function getProduitBoisson(){
+	public function modifier_Produit($idProduit,$designation,$codeProduit,$prixUnitaire){
+		$sql = "UPDATE Produit SET designation= %s , prixUnitaire = %s , codeProduit = %s  where idProduit = %s";
+		$sql = sprintf($sql,$this->db->escape($designation),$this->db->escape($prixUnitaire),$this->db->escape($codeProduit),$this->db->escape($idProduit));
+		$this->db->query($sql);
+	}
 
-		$sql = 'SELECT * FROM Produit WHERE idCategorie=2';
-		$query = $this->db->query($sql);
+	public function recherche_Produit($produit){
+		
+		$sql = 'SELECT * FROM Produit where designation like %%s% ';
+		$query = $this->db->query($sql,$this->db->escape($produit));
 		$produit = array();
 		foreach ($query->result_array() as $key) {
 			$produit[] = $key;
