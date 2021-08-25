896f8bcca49d39cbfc965bda7fe2a95631687dc7
TresorNomenjanahary <80815925+TresorNomenjanahary@users.noreply.github.com>
2021-08-25 05:56:33 +0300

    Add files via upload

--- application/views/SaisieCaisse.php
+++ application/views/SaisieCaisse.php
@@ -1,7 +1,7 @@
 
           <div class="col-md-12">
 
-<form method="post" action="Valider_Commande">
+<form method="post" action="ValidationCommande">
   <p> <select name="produit">
         <?php for ($i=0; $i < count($liste_Produit) ; $i++) { ?>
          <option value="<?php echo $liste_Produit[$i]['idProduit']; ?>"><?php echo $liste_Produit[$i]['designation']; ?></option>                
