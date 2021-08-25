9b3b90f69816d423c6ffc08e4416642f9f651eb7
TresorNomenjanahary <80815925+TresorNomenjanahary@users.noreply.github.com>
2021-08-25 11:09:12 +0300

    Add files via upload

--- application/controllers/Load.php
+++ application/controllers/Load.php
@@ -20,57 +20,97 @@ class Load extends Base_Controller {
 	 */
 	public function __construct(){
 		parent::__construct();
-
 	}
 
-	/*public function Test_Login(){
+	public function index(){
+		$this->load->helper('assets');
+		$data['template'] = 'index.html';
+		$this->load->view('template_accueil',$data);
+		
+	}
+
+	public function ValidationLogin(){
 		$this->load->helper('assets');
 
-		$email = $this->input->post('email');
-		$mdp = $this->input->post('mdp');
+		$email = $this->input->post('loginAdmin');
+		$mdp = $this->input->post('mdpAdmin');
 
-		if(){
-			$data['template'] = 'accueil.php';
+		if($mdp =='' || $email =='' ){
+			$data['template'] = 'accueilAdmin.php';
 			$this->load->view('template',$data);
 		}else{
-			$data['template'] = 'login.php';
+			$data['template'] = 'loginAdmin.php';
 			$this->load->view('template',$data);
 		}
-	}*/
+	}
+
 	public function template($page,$data){
 		
-		$data['template'] = $page.'.php';
+		$data['templateFront'] = $page.'.php';
 		$this->load->view('template',$data);
 	}
-	public function Saisie(){
 
-		$this->load->model('produit');
-		$this->load->model('achat');
+	public function template2($page,$data){
+		
+		$data['templateFront2'] = $page.'.php';
+		$this->load->view('template',$data);
+	}
+
+	public function template3($page,$data){
+		
+		$data['templateFront3'] = $page.'.php';
+		$this->load->view('template',$data);
+	}
+
+	public function SaisieCaisse(){
+
+		$this->load->model('Produit');
+		$this->load->model('Achat');
 		$caisse = $this->session->userdata('caisse');
 		$data['caisse'] = $caisse;
-		$data['liste_produit'] = $this->produit->get_All_Produit();
-		$data['liste_achat'] = $this->achat->getAchat($caisse);
-		$this->template('saisie',$data);
+		$data['liste_Produit'] = $this->Produit->get_All_Produit();
+		$data['liste_Achat'] = $this->Achat->getAchat($caisse);
+		$this->template('SaisieCaisse',$data);
 	}
-	
-	public function Valider_Commande(){
 
-		$this->load->model('achat');
-		$this->load->model('produit');
+	public function ValidationCommande(){
 
-		$idProduit = $this->input->post('produit');
+		$this->load->model('Achat');
+		$this->load->model('Produit');
+
+		$idProduit = $this->input->post('Produit');
 		$quantite = $this->input->post('quantite');
 		$caisse = $this->session->userdata('caisse');
 
 		$this->achat->insert_Achat($caisse,$idProduit,$quantite);
 
 		$data['caisse'] = $caisse;
-		$data['liste_produit'] = $this->produit->get_All_Produit();
-		$data['liste_achat'] = $this->achat->getAchat($caisse);
+		$data['liste_Produit'] = $this->produit->get_All_Produit();
+		$data['liste_Achat'] = $this->achat->getAchat($caisse);
 		$this->template('saisie',$data);
 	}
-	public function Changer_Caisse(){
-		$this->session->sess_destroy();
-		redirect(site_url());
+
+	public function get_Produit(){
+		$this->load->model('Produit');
+		$data['Produit'] = $this->Produit->get_All_Produit();
+		$this->template('listeProduit',$data);
+	}
+
+	public function get_ProduitIndex1(){
+		$this->load->model('Produit');
+		$data['Produit1'] = $this->Produit->getProduitdivers();
+		$this->template('Produit',$data);
+	}
+
+	public function get_ProduitIndex2(){
+		$this->load->model('Produit');
+		$data['Produit2'] = $this->Produit->getProduitBoisson();
+		$this->template('Produit',$data);
+	}
+
+	public function get_ProduitIndex3(){
+		$this->load->model('Produit');
+		$data['Produit3'] = $this->Produit->getProduitLegume();
+		$this->template('Produit',$data);
 	}
 }
